e1853cbe86cae014840f790dc75c496338a548af
MeikaFreckelton <meikafreckelton@gmail.com>
2020-11-14 12:33:09 +1100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const App = () => {
+
+  const [percentageVal, setPercentageVal] = useState(0)
+  const [billVal, setBillVal] = useState(0)
+  const [result, setResult] = useState(0)
+
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    const tip = (percentageVal/100) * billVal
+    const total = tip + parseInt(billVal)
+    setResult(total)
+  }
+
+  const handleInputChange = (e) => {
+    if (e.target.name ==="percentageValue"){
+      setPercentageVal(e.target.value)
+    } else if (e.target.name ==="billValue"){
+      setBillVal(e.target.value)
+    }
+  }
+
+
+
+  return(
+    <div>
+      <h1>bill and tip</h1>
+      <h3>Total: {result}</h3>
+
+      <form onSubmit={handleSubmit}>
+          <label>Bill Amount: </label><br/>
+          $<input 
+          type="number" 
+          name="billValue"
+          onChange={handleInputChange}
+          ></input><br/><br/>
+
+          <label>Tip percentage:</label><br/>
+          <input 
+          type="number"
+          name="percentageValue"
+          onChange={handleInputChange}
+          ></input>%<br/><br/>
+
+          <input type="submit"></input>
+        </form>
     </div>
-  );
+  )
 }
+
+
+
 
 export default App;
